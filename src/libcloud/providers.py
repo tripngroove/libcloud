aa8322e55658eb5ec8b01e6439808d714aa86729
Alex Polvi <alex@polvi.net>
2009-07-22 13:34:08 -0700

    moving stuff around

--- src/libcloud/providers.py
+++ src/libcloud/providers.py
@@ -2,10 +2,10 @@ from libcloud.types import Provider, ProviderCreds
 
 def get_provider(provider):
   if provider == Provider.DUMMY:
-    from libcloud.dummy import DummyProvider
+    from libcloud.drivers.dummy import DummyProvider
     return DummyProvider
   if provider == Provider.EC2:
-    from libcloud.ec2 import EC2Provider
+    from libcloud.drivers.ec2 import EC2Provider
     return EC2Provider
 
 def connect(provider, key, secret=None):
